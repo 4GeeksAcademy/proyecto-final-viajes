e0664420ed3495c929b81dfd731e678f91a7021f
Tony1406 <142957069+Tony1406@users.noreply.github.com>
2024-02-08 01:31:06 +0000

    MisRutas

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,11 +1,9 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
-import { BackendURL } from "./component/backendURL";
 
-import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
+import { InicioBusqueda } from "./pages/InicioBusqueda";
+import { GaleriaTours } from "./pages/GaleriaTours";
+import { MisRutas } from "./pages/MisRutas";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -17,21 +15,16 @@ const Layout = () => {
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
 
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
-
     return (
         <div>
             <BrowserRouter basename={basename}>
-                <ScrollToTop>
                     <Navbar />
                     <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
-                        <Route element={<h1>Not found!</h1>} />
+                        <Route element={<InicioBusqueda />} path="/" />
+                        <Route element={<GaleriaTours />} path="/tours" />
+                        <Route element={<MisRutas />} path="/misrutas" />
                     </Routes>
                     <Footer />
-                </ScrollToTop>
             </BrowserRouter>
         </div>
     );
---
+++ src/front/js/pages/InicioBusqueda.js
@@ -0,0 +1,62 @@
+import React from "react";
+import "../../styles/home.css";
+
+export const InicioBusqueda = () => {
+
+	return (
+		<div className="bg-black">
+			{/* BUSQUEDA */}
+
+
+
+			{/* HOMBRE CAMINANDO */}
+			<figure className="text-center py-5">
+				<i class="fa-solid fa-person-walking text-light display-2"></i>
+				<blockquote className="blockquote text-light py-4">
+					<p className="menu">"Hacemos tu viaje mas interesante"</p>
+				</blockquote>
+			</figure>
+
+			{/* SERVICIOS */}
+			<p className="servicios menu text-light">SERVICIOS</p>
+
+
+			<div className="card-group row menu mx-auto py-5">
+				<div className="col-4 d-flex servicio1">
+					<div className="card bg-black">
+						<img src="https://media.istockphoto.com/id/1285957020/es/foto/explosi%C3%B3n-de-una-bombilla-el%C3%A9ctrica-tradicional-disparo-tomado-a-alta-velocidad.jpg?s=612x612&w=0&k=20&c=TC2l24YAi38mV1AYLAX0vVz3GLJPqop8b_g_ZUD-DyM=" className="card-img-top border border-white " alt="fotico" />
+						<div className="card-body">
+							<h5 className="card-title text-center text-light">SERVICIO 1</h5>
+							<p className="card-text text-light">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+						</div>
+					</div>
+				</div>
+
+				<div className="col-4 d-flex servicio2">
+					<div className="card bg-black">
+						<img src="https://media.istockphoto.com/id/1285957020/es/foto/explosi%C3%B3n-de-una-bombilla-el%C3%A9ctrica-tradicional-disparo-tomado-a-alta-velocidad.jpg?s=612x612&w=0&k=20&c=TC2l24YAi38mV1AYLAX0vVz3GLJPqop8b_g_ZUD-DyM=" className="card-img-top border border-white" alt="fotico" />
+						<div className="card-body">
+							<h5 className="card-title text-center text-light">SERVICIO 1</h5>
+							<p className="card-text text-light">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+						</div>
+					</div>
+				</div>
+				<div className="col-4 d-flex servicio3">
+					<div className="card bg-black">
+						<img src="https://media.istockphoto.com/id/1285957020/es/foto/explosi%C3%B3n-de-una-bombilla-el%C3%A9ctrica-tradicional-disparo-tomado-a-alta-velocidad.jpg?s=612x612&w=0&k=20&c=TC2l24YAi38mV1AYLAX0vVz3GLJPqop8b_g_ZUD-DyM=" className="card-img-top border border-white" alt="fotico" />
+						<div className="card-body">
+							<h5 className="card-title text-center text-light">SERVICIO 1</h5>
+							<p className="card-text text-light">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			{/* PLANES */}
+			<p className="servicios menu text-light">PLANES</p>
+
+			
+
+		</div>
+	);
+};
