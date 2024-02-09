570940f922ad39ecf12abfc2e0ede359e4d91f0c
Raul Madero <127880270+Raul-madero@users.noreply.github.com>
2024-02-09 00:36:21 +0000

    Modifique el archivo inicio busqueda para agregar el componente Jumbotron

--- src/front/js/pages/InicioBusqueda.js
+++ src/front/js/pages/InicioBusqueda.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "../../styles/home.css";
+import Jumbotron from "../component/Jumbotron";
 
 export const InicioBusqueda = () => {
 
 	return (
 		<div className="bg-black">
 			{/* BUSQUEDA */}
-
+			<Jumbotron />
 
 
 			{/* HOMBRE CAMINANDO */}
