ce6e5416620966a97a2314e012c5596087073f8a
Raul Madero <127880270+Raul-madero@users.noreply.github.com>
2024-02-10 02:43:04 +0000

    Ciudad por pais

--- src/front/js/component/Header.js
+++ src/front/js/component/Header.js
@@ -15,8 +15,8 @@ const Header = () => {
                 </div>
                 <div>
                     <NavbarToggle aria-controls="basic-navbar-nav" className="border border-light justify-self-end">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#fff" class="bi bi-list" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#fff" className="bi bi-list" viewBox="0 0 16 16">
+                            <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
                         </svg>
                     </NavbarToggle>
                     <NavbarCollapse id="basic-navbar-nav" className="justify-self-end">
