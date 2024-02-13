d9ef0bda42c9d8ff3aabb52a826f933b598ad18b
Freddy2793 <51304209+Freddy2793@users.noreply.github.com>
2024-02-13 03:20:22 +0000

    inicioSecion

--- src/front/js/pages/Iniciarsesion.js
+++ src/front/js/pages/Iniciarsesion.js
@@ -6,7 +6,21 @@ export const Iniciarsesion = () => {
 
 	return (
 		<div>
-			<h1> hola mundo</h1>
+			<form>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">CORRE</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" /> 
+           
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">CONTRASEÑA</label>
+            <input type="password" class="form-control" id="exampleInputPassword1"/>
+        </div>
+        
+        
+        <button type="submit" class="btn btn-primary">INICIAR SESION</button>
+    </form>
+
 		</div>
 	);
 };
