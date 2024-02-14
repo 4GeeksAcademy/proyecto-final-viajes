d854969491e510274343e6ec64457675eca4f4c9
Tony1406 <142957069+Tony1406@users.noreply.github.com>
2024-02-14 00:31:52 +0000

    modal

--- src/front/js/pages/InicioBusqueda.js
+++ src/front/js/pages/InicioBusqueda.js
@@ -27,7 +27,7 @@ export const InicioBusqueda = () => {
 					<div className="card bg-black">
 						<img src="https://media.istockphoto.com/id/1285957020/es/foto/explosi%C3%B3n-de-una-bombilla-el%C3%A9ctrica-tradicional-disparo-tomado-a-alta-velocidad.jpg?s=612x612&w=0&k=20&c=TC2l24YAi38mV1AYLAX0vVz3GLJPqop8b_g_ZUD-DyM=" className="card-img-top border border-white " alt="fotico" />
 						<div className="card-body">
-							<h5 className="card-title text-center text-light">SERVICIO 1</h5>
+							<h5 className="card-title text-center text-light">ORGANIZAMOS</h5>
 							<p className="card-text text-light">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
 						</div>
 					</div>
@@ -57,36 +57,32 @@ export const InicioBusqueda = () => {
 			<p className="servicios menu text-light">PLANES</p>
 
 			<div className="container-fluid row menu ">
-				{/* <div className="col-1">
 
-				</div> */}
-				<div className="col-3 border border-3 border-white mx-5 rounded my-5">
-					<div className="bg-black text-light text-center mes rounded">
+				<div className="col-3 border border-3 border-white mx-5 redondeado my-5">
+					<div className="bg-black text-light text-center mes redondeado">
 						1 MES
 					</div>
 					<div className="bg-white text-center mensualidad rounded-bottom">
 						7,99 $ / AL MES
 					</div>
 				</div>
-				<div className="col-3 border border-3 border-white mx-5 rounded my-5">
-					<div className="bg-black text-light text-center mes rounded">
+				<div className="col-3 border border-3 border-white mx-5 redondeado my-5">
+					<div className="bg-black text-light text-center mes redondeado">
 						1 MES
 					</div>
 					<div className="bg-white text-center mensualidad rounded-bottom">
 						7,99 $ / AL MES
 					</div>
 				</div>
-				<div className="col-3 border border-3 border-white mx-5 rounded my-5">
-					<div className="bg-black text-light text-center mes rounded">
+				<div className="col-3 border border-3 border-white mx-5 redondeado my-5">
+					<div className="bg-black text-light text-center mes redondeado">
 						1 MES
 					</div>
 					<div className="bg-white text-center mensualidad rounded-bottom">
 						7,99 $ / AL MES
 					</div>
 				</div>
-				{/* <div className="col-1">
-
-				</div> */}
+				
 			</div>
 		</div>
 	);
