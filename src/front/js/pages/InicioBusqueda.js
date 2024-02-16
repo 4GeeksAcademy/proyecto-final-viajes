019e3448aa303ba5ee3a7d527eece6f562c7d639
Raul Madero <127880270+Raul-madero@users.noreply.github.com>
2024-02-16 15:23:27 +0000

    Correccion pagina de inicio integracion de mercado pago en los planes y desaparicion del area cuando inicia sesion

--- src/front/js/pages/InicioBusqueda.js
+++ src/front/js/pages/InicioBusqueda.js
@@ -4,7 +4,8 @@ import Jumbotron from "../component/Jumbotron";
 import { Context } from "../store/appContext";
 
 export const InicioBusqueda = () => {
-	const {store, actions} = useContext(Context)
+	const { store, actions } = useContext(Context)
+	const token = localStorage.getItem("token")
 	const pagar = async (plan) => {
 		await actions.mercadoPago(plan)
 		let url = await store.mercadoPago.init_point
@@ -14,8 +15,6 @@ export const InicioBusqueda = () => {
 		<div className="bg-black">
 			{/* BUSQUEDA */}
 			<Jumbotron />
-
-
 			{/* HOMBRE CAMINANDO */}
 			<figure className="text-center py-5">
 				<i className="fa-solid fa-person-walking text-light display-2"></i>
@@ -23,10 +22,8 @@ export const InicioBusqueda = () => {
 					<p className="menu">"Hacemos tu viaje mas interesante"</p>
 				</blockquote>
 			</figure>
-
 			{/* SERVICIOS */}
 			<p className="servicios menu text-light">SERVICIOS</p>
-
 			<div className="card-group row menu mx-auto py-5">
 				<div className="col-4 d-flex servicio1">
 					<div className="card bg-black">
@@ -37,11 +34,9 @@ export const InicioBusqueda = () => {
 						</div>
 					</div>
 				</div>
-
 				<div className="col-4 d-flex servicio2">
 					<div className="card bg-black">
 						<img src="https://i.ibb.co/jGbxYZ7/pexels-andrea-piacquadio-826349.jpg" className="card-img-top border border-2 border-white rounded-1" alt="fotico" />
-						
 						<div className="card-body">
 							<h5 className="card-title text-light">ELIGE O GUARDA</h5>
 							<p className="card-text text-light">Entre una serie de tours preestablecidos para garantizar tu comodidad.</p>
@@ -58,38 +53,38 @@ export const InicioBusqueda = () => {
 					</div>
 				</div>
 			</div>
-
 			{/* PLANES */}
-			<p className="servicios menu text-light">PLANES</p>
-
-			<div className="container-fluid row menu ">
-
-				<div className="col-3 border border-3 border-white mx-5 redondeado my-5">
-					<div className="bg-black text-light text-center mes redondeado">
-						1 MES
+			{token ? <></> :
+				<>
+					<p className="servicios menu text-light">PLANES</p>
+					<div className="container-fluid row menu ">
+						<div className="col-3 border border-3 border-white mx-5 redondeado my-5">
+							<div className="bg-black text-light text-center mes redondeado">
+								1 MES
+							</div>
+							<button onClick={() => pagar(1.99)} className="bg-white text-center mensualidad rounded-bottom w-100 mx-0">
+								1,99 $ / AL MES
+							</button>
+						</div>
+						<div className="col-3 border border-3 border-white mx-5 redondeado my-5">
+							<div className="bg-black text-light text-center mes redondeado">
+								3 MESES
+							</div>
+							<button onClick={() => pagar(4.99)} className="bg-white text-center mensualidad rounded-bottom w-100 mx-0">
+								4,99 $ / AL MES
+							</button>
+						</div>
+						<div className="col-3 border border-3 border-white mx-5 redondeado my-5">
+							<div className="bg-black text-light text-center mes redondeado">
+								1 AÑO
+							</div>
+							<button onClick={() => pagar(19.99)} className="bg-white text-center mensualidad rounded-bottom w-100 mx-0">
+								19,99 $ / AL MES
+							</button>
+						</div>
 					</div>
-					<div onClick={() => pagar(1.99)} className="bg-white text-center mensualidad rounded-bottom">
-						1,99 $ / AL MES
-					</div>
-				</div>
-				<div className="col-3 border border-3 border-white mx-5 redondeado my-5">
-					<div className="bg-black text-light text-center mes redondeado">
-						3 MESES
-					</div>
-					<div className="bg-white text-center mensualidad rounded-bottom">
-						4,99 $ / AL MES
-					</div>
-				</div>
-				<div className="col-3 border border-3 border-white mx-5 redondeado my-5">
-					<div className="bg-black text-light text-center mes redondeado">
-						1 AÑO
-					</div>
-					<div className="bg-white text-center mensualidad rounded-bottom">
-						19,99 $ / AL MES
-					</div>
-				</div>
-				
-			</div>
+				</>
+			}
 		</div>
 	);
 };
--- src/front/js/pages/MisRutas.js
+++ src/front/js/pages/MisRutas.js
@@ -14,42 +14,20 @@ export const MisRutas = () => {
 
 			{/* SERVICIOS */}
 			<p className="RUTAS menu text-light">MIS RUTAS</p>
-
-
 			<div className="card-group row menu mx-auto py-5">
-				
-				<div className="col-4 d-flex RUTA">
-					<div className="card bg-black">
-						<img src="https://media.istockphoto.com/id/1285957020/es/foto/explosi%C3%B3n-de-una-bombilla-el%C3%A9ctrica-tradicional-disparo-tomado-a-alta-velocidad.jpg?s=612x612&w=0&k=20&c=TC2l24YAi38mV1AYLAX0vVz3GLJPqop8b_g_ZUD-DyM=" className="card-img-top border border-white " alt="fotico" />
-						<div className="card-body">
-							<h5 className="card-title text-center text-light">PLAZA MAYOR, Madrid, España</h5>
-							<p className="card-text text-light pt-4">Distancia: </p>
-							<p className="card-text text-light">Tiempo: </p>
-							<input type="checkbox" id="check" className="border border-3 "/>
+					{store.misRutas.map((ruta, index) => (
+						<div key={index} className="col d-flex RUTA">
+							<div className="card bg-black">
+								<img src={ruta.imagen} className="card-img-top border border-white " alt="fotico" />
+								<div className="card-body">
+									<h5 className="card-title text-center text-light">{ruta.nombre_de_ruta}</h5>
+									<p className="card-text text-light pt-4">Distancia: {ruta.distancia}</p>
+									<p className="card-text text-light">Tiempo: {ruta.tiempo_de_recorrido}</p>
+									<input type="checkbox" id="check" className="border border-3 "/>
+								</div>
+							</div>
 						</div>
-					</div>
-				</div>
-
-				{/* <div className="col-4 d-flex RUTA">
-					<div className="card bg-black">
-						<img src="https://media.istockphoto.com/id/1285957020/es/foto/explosi%C3%B3n-de-una-bombilla-el%C3%A9ctrica-tradicional-disparo-tomado-a-alta-velocidad.jpg?s=612x612&w=0&k=20&c=TC2l24YAi38mV1AYLAX0vVz3GLJPqop8b_g_ZUD-DyM=" className="card-img-top border border-white" alt="fotico" />
-						<div className="card-body">
-							<h5 className="card-title text-center text-light">PLAZA MAYOR, Madrid, España</h5>
-							<p className="card-text text-light pt-4">Distancia: </p>
-							<p className="card-text text-light">Tiempo: </p>
-						</div>
-					</div>
-				</div>
-				<div className="col-4 d-flex RUTA">
-					<div className="card bg-black">
-						<img src="https://media.istockphoto.com/id/1285957020/es/foto/explosi%C3%B3n-de-una-bombilla-el%C3%A9ctrica-tradicional-disparo-tomado-a-alta-velocidad.jpg?s=612x612&w=0&k=20&c=TC2l24YAi38mV1AYLAX0vVz3GLJPqop8b_g_ZUD-DyM=" className="card-img-top border border-white" alt="fotico" />
-						<div className="card-body">
-							<h5 className="card-title text-center text-light">PLAZA MAYOR, Madrid, España</h5>
-							<p className="card-text text-light pt-4">Distancia: </p>
-							<p className="card-text text-light">Tiempo: </p>
-						</div>
-					</div>
-				</div> */}
+					))}
 			</div>
 		</div>
 	);
